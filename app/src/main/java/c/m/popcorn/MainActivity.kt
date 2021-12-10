ebfbe0a2166c6cde9d0eb10937292910d86fdc7a
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-10 21:39:43 +0700

    new files

---
+++ app/src/main/java/c/m/popcorn/MainActivity.kt
@@ -0,0 +1,38 @@
+package c.m.popcorn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import c.m.popcorn.ui.theme.PopCornTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PopCornTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PopCornTheme {
+        Greeting("Android")
+    }
+}
