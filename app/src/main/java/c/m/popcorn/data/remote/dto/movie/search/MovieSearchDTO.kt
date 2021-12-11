5165210edf33ddd2e39f03abf18cfdae392220e8
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-11 11:41:08 +0700

    writing Data Transfer Object / Model data from API for MOVIE

---
+++ app/src/main/java/c/m/popcorn/data/remote/dto/movie/search/MovieSearchDTO.kt
@@ -0,0 +1,14 @@
+package c.m.popcorn.data.remote.dto.movie.search
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieSearchDTO(
+    @SerializedName("page")
+    val page: Int? = 0,
+    @SerializedName("results")
+    val results: List<MovieSearchResultsDTO> = emptyList(),
+    @SerializedName("total_results")
+    val totalResults: Int? = 0,
+    @SerializedName("total_pages")
+    val totalPages: Int? = 0
+)
