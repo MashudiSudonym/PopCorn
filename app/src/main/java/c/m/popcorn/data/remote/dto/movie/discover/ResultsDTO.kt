02ab4a3a35a949bdb4b4a343efbba25439c72d6c
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-11 08:46:50 +0700

    writing Data Transfer Object / Model data from API

--- app/src/main/java/c/m/popcorn/data/remote/dto/movie/discover/ResultsDTO.kt
+++ app/src/main/java/c/m/popcorn/data/remote/dto/movie/discover/ResultsDTO.kt
@@ -11,5 +11,24 @@ data class ResultsDTO(
     val overview: String? = "",
     @SerializedName("release_date")
     val releaseDate: String? = "",
-    // TODO : Write next data from https://developers.themoviedb.org/3/discover/movie-discover
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>? = emptyList(),
+    @SerializedName("id")
+    val id: Int? = 0,
+    @SerializedName("original_title")
+    val originalTitle: String? = "",
+    @SerializedName("original_language")
+    val originalLanguage: String? = "",
+    @SerializedName("title")
+    val title: String? = "",
+    @SerializedName("backdrop_path")
+    val backdropPath: String? = "",
+    @SerializedName("popularity")
+    val popularity: Double? = 0.0,
+    @SerializedName("vote_count")
+    val voteCount: Int? = 0,
+    @SerializedName("video")
+    val video: Boolean = false,
+    @SerializedName("vote_average")
+    val voteAverage: Double? = 0.0
 )
