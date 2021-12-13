3e24ebdedb8a443bd3799d15ec85a1a1d403b305
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-13 16:52:18 +0700

    initialize local database

--- app/src/main/java/c/m/popcorn/data/local/PopCornLocalDatabase.kt
+++ app/src/main/java/c/m/popcorn/data/local/PopCornLocalDatabase.kt
@@ -2,14 +2,15 @@ package c.m.popcorn.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import c.m.popcorn.data.local.movie.MovieDao
 import c.m.popcorn.data.local.movie.entity.MovieDetailEntity
-import c.m.popcorn.data.local.movie.entity.MovieResultsEntity
 
 @Database(
     entities = [MovieDetailEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class PopCornLocalDatabase : RoomDatabase() {
     abstract val movieDao: MovieDao
 }
