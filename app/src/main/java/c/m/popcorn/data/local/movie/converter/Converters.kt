86087e4cf42b896669301fd6ff39dd37067ab805
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-13 16:23:37 +0700

    create json object converter

---
+++ app/src/main/java/c/m/popcorn/data/local/movie/converter/Converters.kt
@@ -0,0 +1,19 @@
+package c.m.popcorn.data.local.movie.converter
+
+import androidx.room.TypeConverter
+import c.m.popcorn.data.util.JsonParser
+import c.m.popcorn.domain.model.movie.detail.MovieGenres
+import com.google.gson.reflect.TypeToken
+
+class Converters(private val jsonParser: JsonParser) {
+    @TypeConverter
+    fun fromGenresJson(json: String): List<MovieGenres> {
+        return jsonParser.fromJson(json, object : TypeToken<List<MovieGenres>>() {}.type)
+            ?: emptyList()
+    }
+
+    @TypeConverter
+    fun toGenresJson(genres: List<MovieGenres>): String {
+        return jsonParser.toJson(genres, object : TypeToken<List<MovieGenres>>() {}.type) ?: ""
+    }
+}
