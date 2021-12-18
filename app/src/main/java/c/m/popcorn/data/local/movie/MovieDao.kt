5b6ce4c186ee9c53b0e723de9cd8061dcda2d589
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-18 21:17:02 +0700

    update movie dao queries

--- app/src/main/java/c/m/popcorn/data/local/movie/MovieDao.kt
+++ app/src/main/java/c/m/popcorn/data/local/movie/MovieDao.kt
@@ -2,11 +2,26 @@ package c.m.popcorn.data.local.movie
 
 import androidx.room.*
 import c.m.popcorn.data.local.movie.entity.MovieDetailEntity
+import c.m.popcorn.data.local.movie.entity.MovieDiscoverEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MovieDao {
     // Movie Discover Queries
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllMovies(movieDiscover: MovieDiscoverEntity)
+
+    @Query("DELETE FROM movie_discover")
+    suspend fun deleteAllMovies()
+
+    @Transaction
+    suspend fun updateAllMovies(movieDiscover: MovieDiscoverEntity) {
+        deleteAllMovies()
+        insertAllMovies(movieDiscover)
+    }
+
+    @Query("SELECT * FROM movie_discover")
+    suspend fun getAllMovies(): Flow<List<MovieDiscoverEntity>>
 
     // Movie Detail Queries
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -22,8 +37,8 @@ interface MovieDao {
     }
 
     @Query("SELECT * FROM movie_detail")
-    suspend fun getLastSeenMovies(): List<MovieDetailEntity>
+    suspend fun getLastSeenMovies(): Flow<List<MovieDetailEntity>>
 
     @Query("SELECT movie_id FROM movie_detail WHERE movie_id = :movieId")
-    suspend fun getMovieDetail(movieId: Int): MovieDetailEntity
+    suspend fun getMovieDetail(movieId: Int): Flow<MovieDetailEntity>
 }
