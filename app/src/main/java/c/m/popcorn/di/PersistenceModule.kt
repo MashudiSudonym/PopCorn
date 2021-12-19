9e8c664a1a2aa2a63d92d7421e7617a2c0d8e3b8
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-19 12:11:30 +0700

    create presistence module

---
+++ app/src/main/java/c/m/popcorn/di/PersistenceModule.kt
@@ -0,0 +1,28 @@
+package c.m.popcorn.di
+
+import android.content.Context
+import androidx.room.Room
+import c.m.popcorn.common.Constants
+import c.m.popcorn.data.local.MovieGenresConverters
+import c.m.popcorn.data.local.PopCornLocalDatabase
+import c.m.popcorn.data.util.GsonParser
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PersistenceModule {
+    @Provides
+    @Singleton
+    fun providePopCornLocalDatabase(@ApplicationContext context: Context): PopCornLocalDatabase {
+        return Room.databaseBuilder(context, PopCornLocalDatabase::class.java, Constants.POPCORN_DB)
+            .addTypeConverter(MovieGenresConverters(GsonParser(Gson())))
+            .allowMainThreadQueries()
+            .build()
+    }
+}
