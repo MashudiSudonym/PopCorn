ac32d8bf01cd572d0fd2102923522515d99d0d96
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-20 12:53:40 +0700

    create use case module

---
+++ app/src/main/java/c/m/popcorn/di/UseCaseModule.kt
@@ -0,0 +1,9 @@
+package c.m.popcorn.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {}
