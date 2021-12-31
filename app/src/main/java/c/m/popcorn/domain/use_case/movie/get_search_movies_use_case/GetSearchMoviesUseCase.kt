b7fd81a3300b90b20d928d36181ecb8149d01d4c
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-12-31 09:49:36 +0700

    create get search movie discover use case

--- app/src/main/java/c/m/popcorn/domain/use_case/movie/get_search_movies_use_case/GetSearchMoviesUseCase.kt
+++ app/src/main/java/c/m/popcorn/domain/use_case/movie/get_search_movies_use_case/GetSearchMoviesUseCase.kt
@@ -1,2 +1,16 @@
 package c.m.popcorn.domain.use_case.movie.get_search_movies_use_case
 
+import androidx.paging.PagingData
+import c.m.popcorn.domain.model.movie.result.MovieResults
+import c.m.popcorn.domain.repository.movie.MovieRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetSearchMoviesUseCase(private val movieRepository: MovieRepository) {
+    suspend operator fun invoke(
+        querySearch: String,
+        token: String,
+        page: Int
+    ): Flow<PagingData<MovieResults>> {
+        return movieRepository.getSearchMovies(token, querySearch, page)
+    }
+}
