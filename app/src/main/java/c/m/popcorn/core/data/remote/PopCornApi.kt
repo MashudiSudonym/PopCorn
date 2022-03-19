e6e995ef4b0e58dd24db4ed30ecae548c2272206
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-03-19 17:33:48 +0700

    change to suspend function

--- app/src/main/java/c/m/popcorn/core/data/remote/PopCornApi.kt
+++ app/src/main/java/c/m/popcorn/core/data/remote/PopCornApi.kt
@@ -11,20 +11,20 @@ import retrofit2.http.Query
 
 interface PopCornApi {
     @GET("/3/discover/movie")
-    fun movieDiscover(
+    suspend fun movieDiscover(
         @Header(Constants.AUTHORIZATION) token: String,
         @Query(Constants.PAGE) page: Int
     ): MovieDiscoverDTO
 
     @GET("/3/search/movie")
-    fun searchMovies(
+    suspend fun searchMovies(
         @Header(Constants.AUTHORIZATION) token: String,
         @Query(Constants.QUERY) querySearch: String,
         @Query(Constants.PAGE) page: Int
     ): MovieSearchDTO
 
     @GET("/3/movie/{movie_id}")
-    fun movieDetail(
+    suspend fun movieDetail(
         @Header(Constants.AUTHORIZATION) token: String,
         @Path(Constants.MOVIE_ID) movieId: Int
     ): MovieDetailDTO
